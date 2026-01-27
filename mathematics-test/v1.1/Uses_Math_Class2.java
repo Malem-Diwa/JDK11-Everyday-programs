84ea313a162b4d7f0bd294fd541a386565596637
MalemDiwa <malem-diwa@localhost.localdomain>
2026-01-27 14:25:40 +0000

    Added A Math test program.

---
+++ mathematics-test/v1.1/Uses_Math_Class2.java
@@ -0,0 +1,10 @@
+public class Uses_Math_Class2 {
+	public static void main(java.lang.String[] args) {
+		byte POWEROF = 4;
+		byte NUMBER = 2;
+		double POWER = java.lang.Math.pow(NUMBER, POWEROF);
+		java.lang.System.out.print("\u001B[2J");
+		java.lang.System.out.print("\u001B[H");
+		java.lang.System.out.print("\u001B[34m" + POWER + "\u001B[0m");
+	}
+}
---
+++ mathematics-test/v1/Uses_Math_Class1.java
@@ -0,0 +1,8 @@
+public class Uses_Math_Class1 {
+	public static void main(java.lang.String[] args) {
+		byte POWEROF = 4;
+		byte NUMBER = 2;
+		double POWER = java.lang.Math.pow(NUMBER, POWEROF);
+		java.lang.System.out.print("\u001B[34m" + POWER + "\u001B[0m");
+	}
+}
